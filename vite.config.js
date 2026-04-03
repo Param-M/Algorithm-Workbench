16e9e6ad232cec925f68f839fe6589e9b1aeadd8
Param <par56am@gmail.com>
2026-04-03 20:22:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Algorithm-Workbench/', 
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
